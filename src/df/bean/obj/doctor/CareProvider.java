44d69767ee3265219bad8cae319a5d727be85105
@xoftspace <nopphadon.cs@gmail.com>
2018-06-25 10:21:56 +0700

    Edit change Double quote

--- src/df/bean/obj/doctor/CareProvider.java
+++ src/df/bean/obj/doctor/CareProvider.java
@@ -412,7 +412,7 @@ public class CareProvider {
                sql = sql + " and PAYMENT_DATE <= '" + endDate + "')";
                sql = sql + " and PM.HOSPITAL_CODE = '" + hospital_code + "'";
                //sql = sql + " and DR.DOCTOR_TAX_CODE = '" + this.getDoctorCode() + "'";
-               sql = sql + " and EXISTS (SELECT DOCTOR_TAX_CODE FROM DOCTOR WHERE HOSPITAL_CODE = '”+ hospital_code +”' AND CODE = '" + this.getDoctorCode() +
+               sql = sql + " and EXISTS (SELECT DOCTOR_TAX_CODE FROM DOCTOR WHERE HOSPITAL_CODE = '"+ hospital_code +"' AND CODE = '" + this.getDoctorCode() +
             		       "' AND DR.DOCTOR_TAX_CODE = DOCTOR_TAX_CODE)";
                sql = sql + " group by DR.DOCTOR_TAX_CODE";
 
